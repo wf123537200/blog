a669e0af1a3a2a2d8f656a7f98d96d05934af51d
zakwu <zakwu@tencent.com>
2020-01-06 16:35:43 +0800

    add /blog 4 test

--- deploy.js
+++ deploy.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var read_file_sync = function (file_path) {
     var data = fs.readFileSync(file_path, 'utf-8');
     console.log('读取文件完成！');
-    data = data.replace(/pl__all\" href=\"/, 'pl__all" href="/blog/');
+    data = data.replace(/pl__all\" href=\"/g, 'pl__all" href="/blog/');
 
     // 写入文件内容（如果文件不存在会创建一个文件）
     fs.writeFile(file_path, data, { 'flag': 'w' }, function(err) {
