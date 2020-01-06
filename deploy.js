854f9546bb30d9b6bdae7a62dd77ac5000edcd33
zakwu <zakwu@tencent.com>
2020-01-06 17:40:11 +0800

    add deploy 4 fix refresh ok

--- deploy.js
+++ deploy.js
@@ -1,11 +1,29 @@
 
 var fs = require('fs');
+// 读取 index.html
+var file_path = './index.html'
+var jsFile_ptah = './assets/js/script.js'
+var insertHtml = '';
 
-var read_file_sync = function (file_path) {
+var injectHtmlFn = function (jsFile_ptah) {
+    var data = fs.readFileSync(jsFile_ptah, 'utf-8');
+    data = 'window.plContainer = `' + insertHtml + '`;' + data;
+    console.log(data)
+    console.log('注入完成！');
+    // 写入文件内容（如果文件不存在会创建一个文件）
+    fs.writeFile(jsFile_ptah, data, { 'flag': 'w' }, function(err) {
+        if (err) {
+            throw err;
+        }
+
+        console.log('注入ok');
+    });
+}
+var readIndexHtml = function (file_path) {
     var data = fs.readFileSync(file_path, 'utf-8');
     console.log('读取文件完成！');
     data = data.replace(/pl__all\" href=\"/g, 'pl__all" href="/blog/');
-
+    insertHtml = data.match(/<nav id=\"pl__container\">([\s\S]*)<\/nav>/)[1]
     // 写入文件内容（如果文件不存在会创建一个文件）
     fs.writeFile(file_path, data, { 'flag': 'w' }, function(err) {
         if (err) {
@@ -13,10 +31,9 @@ var read_file_sync = function (file_path) {
         }
 
         console.log('写入文件ok~~');
+        injectHtmlFn(jsFile_ptah)
     });
 }
 
-// 读取 index.html
-var file_path = './index.html'
+readIndexHtml(file_path)
 
-read_file_sync(file_path)
